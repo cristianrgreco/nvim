91a10a6fafc8a1f0b417a31cd8b861d9ad807af1
Cristian Greco <cristianrgreco@gmail.com>
2024-11-10 22:11:01 +0000

    Extract plugins to own files

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,16 +1,5 @@
 return {
   {
-    "stevearc/conform.nvim",
-    event = "BufWritePre",
-    opts = require "configs.conform",
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
-  {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
