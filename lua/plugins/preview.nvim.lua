1454ee994da5e7cd8fed249023409dce48af64b0
Cristian Greco <cristianrgreco@gmail.com>
2024-11-14 09:51:25 +0000

    Add open in GH plugin

--- lua/plugins/preview.nvim.lua
+++ lua/plugins/preview.nvim.lua
@@ -9,12 +9,14 @@ return {
     opts = {
       previewers_by_ft = {
         plantuml = {
+          -- name = "plantuml_text",
+          -- renderer = { type = "buffer" },
           -- name = "plantuml_svg",
           -- renderer = { type = "command", opts = { cmd = { "/Users/cristian/Desktop/imgcat.sh" } } },
-          name = "plantuml_text",
-          renderer = { type = "buffer" },
-          -- name = "plantuml_svg",
-          -- renderer = { type = "command", opts = { cmd = { "open", "-a", "Preview" } } },
+          name = "plantuml_png",
+          renderer = { type = "command", opts = { cmd = { "open", "-a", "Preview" } } },
+          -- name = "plantuml_png",
+          -- renderer = { type = "command", opts = { cmd = { "timg", "-g200x200" } } },
         },
       },
     },
