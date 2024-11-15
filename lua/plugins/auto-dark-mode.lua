ae4179550b05aebe70fd603bc54ad1d137aed057
Cristian Greco <cristianrgreco@gmail.com>
2024-11-15 10:38:33 +0000

    Re-enable auto-dark-mode

--- lua/plugins/auto-dark-mode.lua
+++ lua/plugins/auto-dark-mode.lua
@@ -5,7 +5,7 @@ return {
     require("auto-dark-mode").setup {
       update_interval = 1000,
       set_dark_mode = function()
-        require("nvconfig").base46.theme = "catppuccin"
+        require("nvconfig").base46.theme = "onedark"
         require("base46").load_all_highlights()
       end,
       set_light_mode = function()
