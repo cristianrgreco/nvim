f29bcf82fb7c6a26a7a5365a61db461405a0085b
Cristian Greco <cristianrgreco@gmail.com>
2024-11-16 20:14:48 +0000

    Simplify auto-dark-mode config

--- lua/plugins/auto-dark-mode.lua
+++ lua/plugins/auto-dark-mode.lua
@@ -1,18 +1,15 @@
 return {
   "f-person/auto-dark-mode.nvim",
   event = "VeryLazy",
-  config = function()
-    require("auto-dark-mode").setup {
-      update_interval = 1000,
-      set_dark_mode = function()
-        require("nvconfig").base46.theme = "catppuccin"
-        require("base46").load_all_highlights()
-      end,
-      set_light_mode = function()
-        require("nvconfig").base46.theme = "one_light"
-        require("base46").load_all_highlights()
-      end,
-    }
-    require("auto-dark-mode").init()
-  end,
+  opts = {
+    update_interval = 1000,
+    set_dark_mode = function()
+      require("nvconfig").base46.theme = "catppuccin"
+      require("base46").load_all_highlights()
+    end,
+    set_light_mode = function()
+      require("nvconfig").base46.theme = "one_light"
+      require("base46").load_all_highlights()
+    end,
+  },
 }
