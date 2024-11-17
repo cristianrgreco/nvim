8d2d62eea895df245a16b4aed7a3ac7c5a7415c7
Cristian Greco <cristianrgreco@gmail.com>
2024-11-17 22:28:52 +0000

    Refactor neotest plugin

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,28 +1,22 @@
 return {
-  { "nvim-neotest/neotest-go" },
-  { "haydenmeade/neotest-jest" },
-  { "marilari88/neotest-vitest" },
-
-  { "nvim-neotest/nvim-nio" },
-  { "mfussenegger/nvim-dap" },
-
-  {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "nvim-neotest/nvim-nio",
-      "mfussenegger/nvim-dap",
-      "nvim-lua/plenary.nvim",
-      "antoinemadec/FixCursorHold.nvim",
-      "nvim-treesitter/nvim-treesitter",
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/neotest-go",
+    "haydenmeade/neotest-jest",
+    "marilari88/neotest-vitest",
+    "nvim-neotest/nvim-nio",
+    "mfussenegger/nvim-dap",
+    "nvim-neotest/nvim-nio",
+    "mfussenegger/nvim-dap",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {
+    adapters = {
+      ["neotest-go"] = {},
+      ["neotest-jest"] = {},
+      ["neotest-vitest"] = {},
     },
-    config = function()
-      require("neotest").setup {
-        adapters = {
-          require "neotest-go",
-          require "neotest-jest",
-          require "neotest-vitest",
-        },
-      }
-    end,
   },
 }
